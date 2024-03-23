0c4263cee3cba01e0d7dc79640be3edc9f487a8d
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-03-22 19:56:27 -0500

    all

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div>
+    <p>
+      Buen dia! 🌑 Puedo ver por tu historia que estas en el hospital. Con los regalos y las afirmaciones,
+      asumo que la operacion paso bien. :)
+    </p>
+
+    <p>
+      Me alegra que estes bien, de hecho, estoy muy feliz. Llena mi corazon ❤️. Siempre veia la luz.
+    </p>
+
+    <p>
+      Quiero reconectar contigo. Hay muchas noticias que pasaron en mi vida. No tengas mieda escribirme o preguntarme algo. Nunca. 🐻
+      😁
+    </p>
+  </div>
+</main>
+
+<style>
+
+  div {
+    margin: 0 auto;
+    max-width: 120rem;
+    background-color: #fbeddb;
+    padding: 2rem;
+    font-size: 2rem;
+    color: #333;
+    font-family: "Satisfy", cursive;
+  }
+
+
+</style>
